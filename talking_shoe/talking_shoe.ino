3b300a9dcdd8644d60fedd7fe3105f95228e1bb7
moink <theresa.robinson@gmail.com>
2025-09-21 10:23:44 +0200

    Full table with all 3 characters

--- talking_shoe/talking_shoe.ino
+++ talking_shoe/talking_shoe.ino
@@ -5,28 +5,39 @@
 #include "DFRobotDFPlayerMini.h"
 
 // CHARACTER SELECT BUTTON
-#define NUMBER_OF_CHARACTERS 4   
-#define BUTTON_PIN 25
-#define BUTTON_DELAY 200
+constexpr uint8_t NUMBER_OF_CHARACTERS = 3;
+constexpr uint8_t BUTTON_PIN = 25;
+constexpr uint16_t BUTTON_DELAY = 200; // ms
 
 // LIGHT DETECTOR
-#define LIGHT_DETECTOR_PIN 34
-#define LIGHT_THRESHOLD 20   
+constexpr uint8_t LIGHT_DETECTOR_PIN = 34;
+constexpr uint16_t LIGHT_THRESHOLD = 20;
 
 // ACCELEROMETER
-#define ACCELEROMETER_DELAY 100 //ms
-#define MOTION_THRESHOLD 0.3
+constexpr uint16_t ACCELEROMETER_DELAY = 100; // ms
+constexpr float MOTION_THRESHOLD = 0.3f;
 
 // MP3 PLAYER
-#define PLAYER_VOLUME 20
-#define FPSerial Serial1
-#define MP3_LENGTH 3000
+constexpr uint8_t PLAYER_VOLUME = 20; // 1 to 30
+#define FPSerial Serial1   
 
-enum ShoeState {
-  LIGHT_MOVING,
-  DARK_MOVING,
-  LIGHT_STATIONARY,
-  DARK_STATIONARY
+// ARRAY SIZING
+constexpr uint8_t MAX_TRACKS_PER_STATE = 6;
+
+enum class SensorState : uint8_t {
+    LIGHT_STATIONARY,
+    LIGHT_MOVING,
+    DARK_STATIONARY,
+    DARK_MOVING
+};
+
+enum Activity {
+    DOING_NOTHING = 0, 
+    PUTTING_ON,
+    WORN_STANDING_STILL,
+    WALKING, 
+    JUST_REMOVED, 
+    NUM_ACTIVITIES   // always the last entry, sentinel value
 };
 
 Adafruit_ADXL345_Unified ACCELEROMETER = Adafruit_ADXL345_Unified(12345);
@@ -34,11 +45,72 @@ DFRobotDFPlayerMini myDFPlayer;
 volatile int character_selected = 0;
 volatile bool buttonPressed = false;
 
+struct Track {
+    uint16_t track_number;    // DFPlayer track number
+    unsigned long length_ms;  // length of track in milliseconds
+};
+
+struct StateTracks {
+    Track tracks[MAX_TRACKS_PER_STATE];
+    uint8_t number_of_tracks; // how many valid tracks are defined
+};
+
+struct ActivityResult {
+    Activity activity;
+    bool light_transition_handled;
+};
+
+StateTracks track_table[NUMBER_OF_CHARACTERS][NUM_ACTIVITIES] = {
+    // Character 0: Matt
+    {
+        // DOING_NOTHING (0)
+        { { {1, 8277}, {2, 12331}, {4, 7680} }, 3 },
+        // PUTTING_ON (1)
+        { { {3, 4096}, {5, 3925}, {6, 3328} }, 3 },
+        // WORN_STANDING_STILL (2)
+        { { {8, 4096}, {9, 3925}, {11, 3883}, {12, 4139} }, 4 },
+        // WALKING (3)
+        { { {7, 2219}, {13, 3797}, {14, 3627}, {15, 4523} }, 4 },
+        // JUST_REMOVED (4)
+        { { {10, 5675}, {16, 8320}, {17, 3925} }, 3 }
+    },
+
+    // Character 1: Tina
+    {
+        // DOING_NOTHING (0)
+        { { {18, 6415}, {19, 9101}, {20, 6366}, {21, 7211}, {22, 11936} }, 5 },
+        // PUTTING_ON (1)
+        { { {23, 3357}, {24, 7735}, {25, 12697}, {26, 7151}, {27, 5254} }, 5 },
+        // WORN_STANDING_STILL (2)
+        { { {28, 8397}, {29, 7007}, {30, 6006}, {31, 10177}, {32, 3504} }, 5 },
+        // WALKING (3)
+        { { {33, 3034}, {34, 8540}, {35, 7697}, {36, 6742}, {37, 7585}, {38, 4045} }, 6 },
+        // JUST_REMOVED (4)
+        { { {39, 4982}, {40, 2931}, {41, 2931}, {42, 3334} }, 4 }
+    },
+
+    // Character 2: Harry
+    {
+        // DOING_NOTHING (0)
+        { { {43, 10841}, {44, 3762}, {45, 8751} }, 3 },
+        // PUTTING_ON (1)
+        { { {46, 4493}, {47, 2064}, {48, 5068} }, 3 },
+        // WORN_STANDING_STILL (2)
+        { { {49, 5590}, {50, 6139}, {51, 8333} }, 3 },
+        // WALKING (3)
+        { { {52, 6060}, {53, 3997}, {54, 10606} }, 3 },
+        // JUST_REMOVED (4)
+        { { {55, 5068}, {56, 4441}, {57, 13793} }, 3 }
+    }
+};
+
 void setup(){
   setup_console();
   setup_character_select_button();
   setup_accelerometer();
   setup_mp3_player();
+  randomSeed(9);
+  // randomSeed(analogRead(A0) + millis());
 }
 
 void setup_console() {
@@ -65,6 +137,7 @@ void setup_accelerometer(void) {
 }
 
 void setup_mp3_player() {
+  delay(1000);
   FPSerial.begin(9600, SERIAL_8N1, /*rx =*/16, /*tx =*/17);
   if (!myDFPlayer.begin(FPSerial, true, true)) {
     Serial.println("No mp3 player detected. Check your wiring!");
@@ -73,49 +146,37 @@ void setup_mp3_player() {
 }
 
 void loop() {
-  static unsigned long timer = millis();
-  static int last_character_selected = -1;
-  static unsigned long lastPress = 0;
-  if (buttonPressed) {
-    buttonPressed = false;
+    static unsigned long playback_start_time = 0;
+    static unsigned long current_track_length = 0;
+    static unsigned long last_recorded_button_press = 0;
+    static SensorState prev_sensor_state = SensorState::LIGHT_STATIONARY;
+    static bool light_transition_handled = false;
     unsigned long now = millis();
-    if (now - lastPress > BUTTON_DELAY) {
-      character_selected = (character_selected + 1) % NUMBER_OF_CHARACTERS;
-      lastPress = now;
-      Serial.printf("Button count: %d\n", character_selected);
+    if (buttonPressed) {
+        buttonPressed = false;
+        if (now - last_recorded_button_press > BUTTON_DELAY) {
+            character_selected = (character_selected + 1) % NUMBER_OF_CHARACTERS;
+            last_recorded_button_press = now;
+            Serial.printf("Button count: %d\n", character_selected);
+        }
     }
-  }
-  if (millis() - timer > MP3_LENGTH){
-    timer = millis();
-    ShoeState shoe_state = get_shoe_state();
-    switch(shoe_state){
-      case LIGHT_MOVING:
-        Serial.println("Light and moving!\n");
-        myDFPlayer.play(1);
-        break;
-      case DARK_MOVING:
-        Serial.println("Dark and moving!\n");
-        myDFPlayer.play(2);
-        break;
-      case LIGHT_STATIONARY:
-        Serial.println("Light and stationary!\n");
-        myDFPlayer.play(3);
-        break;
-      case DARK_STATIONARY:
-        Serial.println("Dark and stationary!\n");
-        myDFPlayer.play(1);
-        break;
+    if (now - playback_start_time >= current_track_length) {
+        SensorState current_sensor_state = get_sensor_state();
+        ActivityResult activity_result = get_activity(current_sensor_state, prev_sensor_state, light_transition_handled);
+        prev_sensor_state = current_sensor_state;
+        light_transition_handled = activity_result.light_transition_handled;
+        current_track_length = play_random_track(character_selected, activity_result.activity);
+        playback_start_time = now;
     }
-  }
 }
 
-ShoeState get_shoe_state() {
+SensorState get_sensor_state() {
   bool light = is_it_light();
   bool moving = is_it_moving();
-  if (light && moving) return LIGHT_MOVING;
-  if (!light && moving) return DARK_MOVING;
-  if (light && !moving) return LIGHT_STATIONARY;
-  return DARK_STATIONARY;
+  if (light && moving) return SensorState::LIGHT_MOVING;
+  if (!light && moving) return SensorState::DARK_MOVING;
+  if (light && !moving) return SensorState::LIGHT_STATIONARY;
+  return SensorState::DARK_STATIONARY;
 }
 
 
@@ -144,26 +205,52 @@ bool is_it_moving() {
   return total_delta > MOTION_THRESHOLD;
 }
 
-// select track
 
-// select personality
+ActivityResult get_activity(SensorState current, SensorState previous, bool light_transition_handled) {
+    ActivityResult result;
+    // JUST_REMOVED: first light after dark
+    if (!light_transition_handled &&
+        previous == SensorState::DARK_STATIONARY &&
+        (current == SensorState::LIGHT_STATIONARY || current == SensorState::LIGHT_MOVING)) 
+    {
+        result.activity = JUST_REMOVED;
+        result.light_transition_handled = true;  // mark that transition handled
+        return result;
+    }
+    // Normal mappings
+    switch (current) {
+        case SensorState::LIGHT_STATIONARY:
+            result.activity = DOING_NOTHING;
+            break;
+        case SensorState::LIGHT_MOVING:
+            result.activity = PUTTING_ON;
+            break;
+        case SensorState::DARK_STATIONARY:
+            result.activity = WORN_STANDING_STILL;
+            break;
+        case SensorState::DARK_MOVING:
+            result.activity = WALKING;
+            break;
+    }
+    result.light_transition_handled = light_transition_handled;
+    return result;
+}
 
-// play track
 
-// check if still playing
+unsigned long play_random_track(uint8_t character, Activity activity) {
+    uint8_t activity_index = static_cast<uint8_t>(activity);
+    StateTracks &state_tracks = track_table[character][activity_index];
+    uint8_t track_idx = random(state_tracks.number_of_tracks);
+    Track &selected_track = state_tracks.tracks[track_idx];
+    Serial.printf("Playing character %d, activity %d, track %d\n",
+              character, activity_index, selected_track.track_number);
+    myDFPlayer.play(selected_track.track_number);
+    return selected_track.length_ms;
+}
 
-// wait while still playing
 
-// check if momentary switch is pressed
 
-// things we need
-// - more jumpers
 
-// hardware to do
-// - install battery
-// - install into a shoe
-
-// recording
 
 
 
